dad4e5212bf75c992b637451e4929d6aa9dab5db
stefan <stefanbesovic@gmail.com>
2021-01-05 21:16:10 +0100

    Added client, started logic for sending requests and opening listenSocket.

---
+++ Client/Client.cpp
@@ -0,0 +1,151 @@
+// Client.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#pragma comment(lib, "Ws2_32.lib")
+#define _WINSOCK_DEPRECATED_NO_WARNINGS 1
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <ws2tcpip.h>
+#include <winsock2.h>
+#include <stdlib.h>
+#include <time.h>
+#include <queue>
+#include <list>
+#include <unordered_map>
+#include "../PeerToPeerFileTransferFunctions/P2PFTP.h"
+#include "../PeerToPeerFileTransferFunctions/P2PFTP_Structs.h"
+#include "../PeerToPeerFileTransferFunctions/P2PLimitations.h"
+#define DEFAULT_PORT 27016
+#define SERVER_ADDRESS "127.0.0.1"
+
+int port = 50000;
+bool InitializeWindowsSockets();
+
+int main()
+{
+	SOCKET connectSocket = INVALID_SOCKET;
+	SOCKET listenSocket = INVALID_SOCKET;
+
+	srand(time(0));
+
+	//random port
+	int port = (rand() % (50000 - 20000 + 1)) + 20000;
+	char portBuff[5];
+	_itoa(port, portBuff, 10);
+
+	int iResult;
+
+	fd_set readfds;
+	unsigned long mode = 1; 
+
+	if (InitializeWindowsSockets() == false)
+	{
+		return 1;
+	}
+
+	connectSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+	if (connectSocket == INVALID_SOCKET)
+	{
+		printf("socket failed with error: %ld", WSAGetLastError());
+		WSACleanup();
+		return 1;
+	}
+
+	sockaddr_in serverAddress;
+	serverAddress.sin_family = AF_INET;
+	InetPton(AF_INET, TEXT(SERVER_ADDRESS), &serverAddress.sin_addr.s_addr);
+	serverAddress.sin_port = htons(DEFAULT_PORT);
+
+	if (connect(connectSocket, (SOCKADDR*)&serverAddress, sizeof(serverAddress)) == SOCKET_ERROR)
+	{
+		printf("Unable to connect to server.\n");
+		closesocket(connectSocket);
+		WSACleanup();
+		return 1;
+	}
+	
+
+	//LISTEN SOCKET ZA KLIJENTE
+	addrinfo *resultingAddress = NULL;
+	addrinfo hints;
+
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_protocol = IPPROTO_TCP;
+	hints.ai_flags = AI_PASSIVE;
+
+
+	iResult = getaddrinfo(NULL, portBuff, &hints, &resultingAddress);
+	if (iResult != 0)
+	{
+		printf("getaddrinfo failed with error: %d\n", iResult);
+		WSACleanup();
+		return 1;
+	}
+
+	listenSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+	if (listenSocket == INVALID_SOCKET)
+	{
+		printf("socket failed with error: %ld\n", WSAGetLastError());
+		freeaddrinfo(resultingAddress);
+		WSACleanup();
+		return 1;
+	}
+
+	iResult = bind(listenSocket, resultingAddress->ai_addr, (int)resultingAddress->ai_addrlen);
+	if (iResult == SOCKET_ERROR)
+	{
+		printf("bind failed with error: %d\n", WSAGetLastError());
+		freeaddrinfo(resultingAddress);
+		closesocket(listenSocket);
+		WSACleanup();
+		return 1;
+	}
+
+	//IZVLACIM SOCKADDR_IN ZA FILE_REQUEST
+	struct sockaddr_in socketAddress;
+	int socketAddress_len = sizeof(socketAddress);
+
+	if (getsockname(listenSocket, (sockaddr *)&socketAddress, &socketAddress_len) == -1)
+	{
+		printf("getsockname() failed.\n"); return -1;
+	}
+
+	printf("\nPort: %d", (int)ntohs(socketAddress.sin_port));
+	printf("\nIP: %s", inet_ntoa(socketAddress.sin_addr));
+
+
+	printf("Client is up.");
+
+	FILE_REQUEST file;
+	char filename[MAX_FILE_NAME];
+
+	printf("\nEnter a file name: ");
+	gets_s(file.fileName, MAX_FILE_NAME);
+	file.requesterListenAddress = socketAddress;
+
+	printf("\n%s", file.fileName);
+
+	//imam gresku sa linkerom ovde.
+	//SendFileRequest(connectSocket, file);
+
+
+	Sleep(3000);
+
+	return 0;
+}
+
+bool InitializeWindowsSockets()
+{
+	WSADATA wsaData;
+	// Initialize windows sockets library for this process
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+	{
+		printf("WSAStartup failed with error: %d\n", WSAGetLastError());
+		return false;
+	}
+	return true;
+}
